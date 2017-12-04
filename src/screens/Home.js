06cc831847952a8ad2b339afe10960c80495a727
VNPT <vonguyenphuocthinh@gmail.com>
2017-12-04 13:36:13 +0700

    flatlist

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {StyleSheet, Text, View, Button, Image, TouchableOpacity} from 'react-native';
+import {StyleSheet, Text, View, Button, Image, TouchableOpacity, FlatList} from 'react-native';
 
 
 const MenuButton = (props) =>(
@@ -16,12 +16,55 @@ export default class Home extends Component {
         headerLeft: <MenuButton navigation={navigation} />        
     });
 
+    constructor(props){
+        super(props);
+
+        this.state = {
+            loading: false,
+            data: [],
+            page: 1,
+            seed: 1,
+            error: null,
+            refreshing: false
+        };
+    }
+
+    componentDidMount() {
+        console.warn("run ...");
+        this.makeRemoteRequest();
+    }
+
+    makeRemoteRequest = () => {
+        console.warn("makeRemoteRequest");
+        const { page, seed } = this.state;
+        const url = `http://10.2.80.156:82/hcmtucv/work.nsf/DataRest.xsp/datadocument?loai=1&viewname=VwCongViecDangThucHien&idcat=vanthu1/CINOTEC/VN&_=1512355615704`;
+        this.setState({ loading: true });
+    
+        fetch(url)
+          .then(res => res.json())
+          .then(res => {
+            console.warn(this.state.data);  
+            this.setState({
+              data: page === 1 ? res.results : [...this.state.data, ...res.results],
+              error: res.error || null,
+              loading: false,
+              refreshing: false
+            });
+          })
+          .catch(error => {
+            this.setState({ error, loading: false });
+          });
+    };
+
     render() {
         return (
-        <View style={styles.container}>
-            <Text>Home</Text>
-            
-            </View>
+                     <FlatList
+                        data={this.state.data}
+                        renderItem={({ item }) => (
+                            <Text>item.NguoiXuLy</Text>
+                        )}         
+                    />    
+
         );
     }
 }
@@ -32,9 +75,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  icon: {
-    width: 24,
-    height: 24,
-  },
+  }
 });
